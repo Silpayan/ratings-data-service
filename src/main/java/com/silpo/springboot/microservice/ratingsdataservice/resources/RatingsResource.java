05c27a83ec9c0ef6e540da880040fb5316187dc3
silpayan.dutta <silpayan.dutta@workday.com>
2023-05-20 13:55:31 -0700

    Returning UserRatings instead of List as Response so adding corresponding end points for future ref

--- src/main/java/com/silpo/springboot/microservice/ratingsdataservice/resources/RatingsResource.java
+++ src/main/java/com/silpo/springboot/microservice/ratingsdataservice/resources/RatingsResource.java
@@ -1,6 +1,7 @@
 package com.silpo.springboot.microservice.ratingsdataservice.resources;
 
 import com.silpo.springboot.microservice.ratingsdataservice.models.Rating;
+import com.silpo.springboot.microservice.ratingsdataservice.models.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,14 +18,33 @@ public class RatingsResource {
         return new Rating(movieId, "4"); //getter setter is required for the model
     }
 
+    @RequestMapping("usersList/{userId}")
+    public List<Rating> getUserRatingsList(@PathVariable("userId") String userId){
+        //Response : [{"movieId":"1234","rating":"4"},{"movieId":"1235","rating":"5"}]
+        //Since this is list object, if we change the contract consumers would break
+        //So better to use an Object i.e. UserRating
+
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", "4"),
+                new Rating("1235", "5")
+        );
+
+        return ratings;
+    }
+
     @RequestMapping("users/{userId}")
-    public List<Rating> getUserRatings(@PathVariable("userId") String userId){
+    public UserRating getUserRatings(@PathVariable("userId") String userId){
+        //Response : {"userRatings":[{"movieId":"7354","rating":"4"},{"movieId":"46rf8","rating":"3"}]}
+
         List<Rating> ratings = Arrays.asList(
                 new Rating("7354", "4"),
                 new Rating("46rf8", "3")
         );
 
-        return ratings;
+        UserRating userRating = new UserRating();
+        userRating.setUserRatings(ratings);
+
+        return userRating;
     }
 
 
