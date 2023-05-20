6d064fad23bf5753bb17e653be2043fc9094911a
silpayan.dutta <silpayan.dutta@workday.com>
2023-05-20 12:11:53 -0700

    adding new Mapping for users/userId and changing the Rating data type

--- src/main/java/com/silpo/springboot/microservice/ratingsdataservice/resources/RatingsResource.java
+++ src/main/java/com/silpo/springboot/microservice/ratingsdataservice/resources/RatingsResource.java
@@ -5,13 +5,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("/ratingsdata")
 public class RatingsResource {
 
     @RequestMapping("/{movieId}")
-    public Rating getRatingId(@PathVariable("movieId") String movieId){
-
-        return new Rating(movieId, 4); //getter setter is required for the model
+    public Rating getRating(@PathVariable("movieId") String movieId){
+        return new Rating(movieId, "4"); //getter setter is required for the model
     }
+
+    @RequestMapping("users/{userId}")
+    public List<Rating> getUserRatings(@PathVariable("userId") String userId){
+        List<Rating> ratings = Arrays.asList(
+                new Rating("7354", "4"),
+                new Rating("46rf8", "3")
+        );
+
+        return ratings;
+    }
+
+
 }
